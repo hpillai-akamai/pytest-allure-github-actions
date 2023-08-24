a66853531df265f4f1f3ef5f6f5761b492335801
hpillai-akamai <132665917+hpillai-akamai@users.noreply.github.com>
2023-08-25 02:42:56 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,26 +1,6 @@
 import allure
 import pytest
 
-class Calculator:
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b != 0:
-            return a / b
-        else:
-            raise ValueError("Cannot divide by zero")
-
-# Tests
-@pytest.fixture
-def calculator():
-    return Calculator()
 
 @allure.epic("EPIC_1")
 @allure.parent_suite('Parent Suite 1')
@@ -28,43 +8,45 @@ def calculator():
 @pytest.mark.sanity
 @pytest.mark.master
 @pytest.mark.basic_sanity
-@allure.title("Addition Test")
-def test_addition(calculator):
-    with allure.step("Performing addition"):
-        result = calculator.add(2, 3)
-    assert result == 5
+def test_addition():
+    allure.dynamic.suite("SUITE_1")
+    print("addition of 2 and 3")
+    assert 2+3 == 5
 
 @allure.epic("EPIC_1")
 @allure.parent_suite('Parent Suite 1')
 @allure.feature("Calculator Tests")
-@allure.title("Subtraction Test")
-def test_subtraction(calculator):
-    with allure.step("Performing subtraction"):
-        result = calculator.subtract(5, 3)
-    assert result == 2
+@pytest.mark.master
+@pytest.mark.basic_sanity
+def test_subtraction():
+    allure.dynamic.suite("SUITE_1")
+    print("subtraction of 3 from 5")
+    assert 5-3 == 2
 
 @allure.epic("EPIC_1")
 @allure.parent_suite('Parent Suite 1')
 @allure.feature("Calculator Tests")
-@allure.title("Multiplication Test")
-def test_multiplication(calculator):
-    with allure.step("Performing multiplication"):
-        result = calculator.multiply(4, 3)
-    assert result == 12
+@pytest.mark.master
+@pytest.mark.basic_sanity
+def test_multiplication():
+    allure.dynamic.suite("SUITE_1")
+    print("Mulitply 4 and 3")
+    assert 4*3 == 12
 
 @allure.epic("EPIC_1")
 @allure.parent_suite('Parent Suite 1')
 @allure.feature("Calculator Tests")
-@allure.title("Division Test")
-def test_division(calculator):
-    with allure.step("Performing division"):
-        result = calculator.divide(8, 2)
-    assert result == 4
+@pytest.mark.master
+def test_division():
+    allure.dynamic.suite("SUITE_1")
+    pirnt("Divide 8 by 2")
+    assert 8/2 == 4
 
 @allure.epic("EPIC_1")
 @allure.parent_suite('Parent Suite 1')
 @allure.feature("Calculator Tests")
-@allure.title("Test Failure")
+@pytest.mark.basic_sanity
 def test_failure():
+    allure.dynamic.suite("SUITE_1")
     print("TC failure here")
     assert 3 == 4
